f98150fad90853e75e03d7d01a73f0c41e751822
Mason Galland <masongalland@me.com>
2017-11-13 14:14:32 -0700

    successfully connected socket

--- lynda-polling/src/App.js
+++ lynda-polling/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import io from 'socket.io-client'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.connect = this.connect.bind(this)
+  }
+  
 
   
-  componentDidMount() {
+  componentWillMount() {
     this.socket = io("/");
     this.socket.on("connect", this.connect)
   }
 
   connect() {
-    alert("connected: ")
+    alert("connected: " + this.socket.id)
   }
   
 
