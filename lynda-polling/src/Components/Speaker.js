bc3191875af892a7be9f9945d920e225352cd72b
masongalland <masongalland@me.com>
2017-11-21 11:00:41 -0700

    added attendance component to keep track of connected members

--- lynda-polling/src/Components/Speaker.js
+++ lynda-polling/src/Components/Speaker.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Display from './Display';
 import JoinSpeaker from './JoinSpeaker';
+import Attendance from './Attendance';
 
 export default class Speaker extends Component {
     render() {
@@ -9,7 +10,7 @@ export default class Speaker extends Component {
                 <Display if={this.props.status === "connected"}>
                     <Display if={this.props.member.name && this.props.member.type === "speaker"} >
                         <p>Questions</p>
-                        <p>Attendance</p>
+                        <Attendance audience={this.props.audience} />
                     </Display>
 
                     <Display if={!this.props.member.name} >
