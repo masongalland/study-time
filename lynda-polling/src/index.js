5693ed12199257711401b48fb88eaf5c28ddfa90
masongalland <masongalland@me.com>
2017-11-25 12:31:55 -0700

    added sass configuration

--- lynda-polling/src/index.js
+++ lynda-polling/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./styles/main.css";
 import App from "./App";
 
 import { HashRouter } from "react-router-dom";
@@ -10,6 +10,6 @@ import { unregister } from "./registerServiceWorker";
 ReactDOM.render(
     <HashRouter>
         <App />
-    </HashRouter>, 
+    </HashRouter>,
     document.getElementById("root"));
 unregister();
