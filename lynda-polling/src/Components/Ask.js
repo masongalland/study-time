e5844834a9a26c326fabe4a908b6394612534d3a
masongalland <masongalland@me.com>
2017-11-27 16:34:45 -0700

    finished styling for the Ask component

--- lynda-polling/src/Components/Ask.js
+++ lynda-polling/src/Components/Ask.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Display from './Display'
+import Display from "./Display";
 
 export default class Ask extends Component {
   constructor(props) {
@@ -21,39 +21,44 @@ export default class Ask extends Component {
     let choices = Object.keys(this.props.question);
     choices.shift();
     this.setState({ choices: choices, answer: sessionStorage.answer });
-}
+  }
 
-select(choice) {
-    this.setState({answer: choice})
+  select(choice) {
+    this.setState({ answer: choice });
     sessionStorage.answer = choice;
-    this.props.emit('answer', {
-        question: this.props.question,
-        choice: choice
-    })
+    this.props.emit("answer", {
+      question: this.props.question,
+      choice: choice
+    });
   }
 
   render() {
+    const colors = ["#23F0C7", "#4357AD", "#ffe426", "#D81159"];
     const choices = this.state.choices.map((choice, i) => {
-      return ( <button key={i} onClick={()=> this.select(choice)}>
-                {choice}: {this.props.question[choice]}
-               </button>
-      )
+      return (
+        <div
+          className="choice"
+          style={{ backgroundColor: colors[i] }}
+          key={i}
+          onClick={() => this.select(choice)}
+        >
+          {choice}.) {this.props.question[choice]}
+        </div>
+      );
     });
-    console.log(this.props)
-    console.log(this.state)
+    console.log(this.props);
+    console.log(this.state);
     return (
       <div id="currentQuestion">
-
         <Display if={this.state.answer}>
-            <h3>You answered: {this.state.answer}</h3>
-            <p>{this.props.question[this.state.answer]}</p>
+          <h2>You answered: </h2>
+          <p style={{ backgroundColor: colors[this.state.choices.indexOf(this.state.answer)] }} className="choice">{this.state.answer}.)  {this.props.question[this.state.answer]}</p>
         </Display>
 
         <Display if={!this.state.answer}>
-            <h2>{this.props.question.q}</h2>
-            <div>{choices}</div>
+          <h2 className="asked-question">{this.props.question.q}</h2>
+          <div className="choices">{choices}</div>
         </Display>
-
       </div>
     );
   }
